82c65d331b4b1ca724581297c572094de3c04cc0
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-02 13:30:31 +0300

    My first commit!

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryOperator.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryOperator {
     public static void main(String[] args) {
         int b = 16;
-        System.out.println(b == 0 ? 10 : 23);
+        System.out.println(b < 0 ? 10 : 23);
     }
 }
